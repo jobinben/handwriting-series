b86256719a97be9a0842e7199613d474dabd11c7
jobinben <1021137079@qq.com>
2021-12-25 19:56:01 +0800

    添加描述

--- 其他/instanceof.js
+++ 其他/instanceof.js
@@ -1,3 +1,8 @@
+// instanceof 原理: 检测构造函数的 prototype属性在这个实例对象的原型链上
+// 1. 基本类型返回false
+// 2. 获取对象的原型，对象原型为空时，返回false
+// 3. 对象的原型等于构造函数的protoype 返回true
+// 4. 循环向上原型链查找，直到原型链为null或者找到
 function toInstanceof(obj, Constructor) {
     // 基本类型直接返回false
     if((typeof(obj) !== 'object' && typeof(obj) !== 'function') || obj === null){
